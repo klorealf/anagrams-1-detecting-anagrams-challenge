d72fc4cd928fa16bec4a1686087448c951acfe6c
bradsloan & Andreas Bach Landgrebe <pair+bsloan3.grebes15@devbootcamp.com>
2017-08-30 18:52:27 -0400

    add cononical form to shorten code

--- anagrams.rb
+++ anagrams.rb
@@ -1,1 +1,32 @@
+require 'pry'
 
+def anagrams?(word_1, word_2)
+# binding.pry
+  if anagram_canonical_form(word_1)==anagram_canonical_form(word_2)
+    return true
+  else
+    return false
+  end
+  # word_1 = word_1.downcase
+  # word_2 = word_2.downcase
+  # first_word = word_1.split("")
+  # second_word = word_2.split("")
+  # if first_word.length != second_word.length
+  #   return false
+  # end
+  # first_word.each do |letter|
+  #   if second_word.include?(letter)
+  #     second_word.delete_at(second_word.index(letter))
+  #   end
+  # end
+
+  # if second_word.empty?
+  #   return true
+  # else
+  #   return false
+  # end
+end
+
+def anagram_canonical_form(word)
+  word.downcase.split("").sort
+end
--- spec/anagrams_spec.rb
+++ spec/anagrams_spec.rb
@@ -1,4 +1,17 @@
 require_relative '../anagrams'
 
 describe 'anagrams?' do
+
+  it 'check if word is anagram' do
+    expect(anagrams?("lemon", "melon")).to eq true
+  end
+
+  it 'check if word is anagram' do
+    expect(anagrams?("cat", "dog")).to eq false
+  end
+
+  it 'check if word is anagram (case sen)' do
+    expect(anagrams?("god", "Dog")).to eq true
+  end
+
 end
