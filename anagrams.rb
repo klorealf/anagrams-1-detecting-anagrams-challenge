c207f2858f9bb3c2694520c8432180cefebf59b9
klorealf <klorealf@gmail.com>
2017-09-04 23:24:26 -0400

    add .split, .downcase to anagram? method

--- anagrams.rb
+++ anagrams.rb
@@ -3,12 +3,18 @@
 #if string2 includes the letter, return true
 #if string2 doesn't include the letter, return false
 
+
 def anagrams?(string1, string2)
-	if string1.split("").include?(string2.split(""))
+	word1 = string1.downcase.split("")
+	word2 = string2.downcase.split("")
+	if word1.sort == word2.sort
+		return true
 	else
-		false
+		return false
 	end	
 end
+
+p anagrams?('melon', 'lemon')
 
 # def method_name
 	
