2e06f3ce7edd4abb0b4507340c51de3d9b1c1468
klorealf <klorealf@gmail.com>
2017-09-04 22:33:39 -0400

    add rspec test for anagram

--- spec/anagrams_spec.rb
+++ spec/anagrams_spec.rb
@@ -1,4 +1,29 @@
 require_relative '../anagrams'
 
 describe 'anagrams?' do
+	it 'returns true if two strings are anagrams' do
+		anagram = anagrams?("melon", "lemon")
+		expect(anagrams).to eq true
+	end
+
+	it 'returns false if two strings are not anagrams' do
+		anagram = anagrams?("melon", "tiger")
+		expect(anagrams).to eq false
+	end
+
+	it 'returns true if two strings are the same' do
+		anagram = anagrams?("melon", "melon")
+		expect(anagrams).to eq true
+	end
+
+	it 'returns true if either strings is case-insensitive and are anagrams' do
+		anagram = anagrams?("MELON", "lemon")
+		expect(anagrams).to eq true
+	end
+
+	it 'returns true if two strings are anagrams and not valid english words' do
+		anagram = anagrams?("abcde2", "c2abed")
+		expect(anagrams).to eq true
+	end
+
 end
