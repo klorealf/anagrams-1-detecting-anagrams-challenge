c8c1bc113b68fd5ee3f340bbef23367676a5a723
klorealf <klorealf@gmail.com>
2017-09-04 22:34:22 -0400

    add method anagrams? to iterate over strings

--- anagrams.rb
+++ anagrams.rb
@@ -1,1 +1,15 @@
+#INPUT: string1,string2
+#iterate through each letter in string1
+#if string2 includes the letter, return true
+#if string2 doesn't include the letter, return false
 
+def anagrams?(string1, string2)
+	if string1.split("").include?(string2.split(""))
+	else
+		false
+	end	
+end
+
+# def method_name
+	
+# end
