abfaa1d7ebb045f72b62114e7d754e9ae4a186dd
Amber Maillard <amber.maillard23@gmail.com>
2017-09-06 17:08:20 -0400

    Add rspec for edge cases and update anagram checking method

--- anagrams.rb
+++ anagrams.rb
@@ -1,12 +1,12 @@
 # Define a method that takes two strings as parameters
 # Set a variable equal to the each of the strings for a total of 2 varibales
 # Split each of the strings
-# If the two varibales are equal to each other when the split strings are split, then return true
+# If the two variables are equal to each other when the split strings are split, then return true
 # Else return false
 
 def anagrams?(string_1, string_2)
-  word_1 = string_1.split("")
-  word_2 = string_2.split("")
+  word_1 = string_1.downcase.split("")
+  word_2 = string_2.downcase.split("")
   if word_1.sort == word_2.sort
     return true
   else
--- spec/anagrams_spec.rb
+++ spec/anagrams_spec.rb
@@ -9,6 +9,23 @@ describe 'anagrams?' do
   it 'returns false if the two words are not anagrams' do
       expect(anagrams?("strawberry", "apple")).to eq (false)
     end
+
+  it 'returns true if the two words are anagrams and include capitalization' do
+    expect(anagrams?("MElon", "lemoN")).to eq (true)
+  end
+
+  it 'returns true if the two words are anagrams regardless of word order' do
+    expect(anagrams?("lemon", "melon")).to eq (true)
+  end
+
+  it 'returns true if the two words are the same word' do
+    expect(anagrams?("lemon", "lemon")).to eq (true)
+  end
+
+  it 'returns true if the two words contain non-English characters' do
+    expect(anagrams?("l3mon", "m3lon")).to eq (true)
+  end
+
 end
 
 
