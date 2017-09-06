3deba619d8adececa80dcb62b31eeb48adb9a4d1
Amber Maillard <amber.maillard23@gmail.com>
2017-09-06 17:02:43 -0400

    Add base method for detecting anagrams along with updated rspec

--- anagrams.rb
+++ anagrams.rb
@@ -1,1 +1,15 @@
+# Define a method that takes two strings as parameters
+# Set a variable equal to the each of the strings for a total of 2 varibales
+# Split each of the strings
+# If the two varibales are equal to each other when the split strings are split, then return true
+# Else return false
 
+def anagrams?(string_1, string_2)
+  word_1 = string_1.split("")
+  word_2 = string_2.split("")
+  if word_1.sort == word_2.sort
+    return true
+  else
+    return false
+  end
+end
--- spec/anagrams_spec.rb
+++ spec/anagrams_spec.rb
@@ -1,4 +1,15 @@
 require_relative '../anagrams'
 
 describe 'anagrams?' do
+
+  it 'returns true if the two words are anagrams' do
+    expect(anagrams?("melon", "lemon")).to eq (true)
+  end
+
+  it 'returns false if the two words are not anagrams' do
+      expect(anagrams?("strawberry", "apple")).to eq (false)
+    end
 end
+
+
+
