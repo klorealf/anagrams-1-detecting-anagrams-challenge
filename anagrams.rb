37ac2990b6a138b2912511a73d484a9a1821e684
Chris <cpcamp2@gmail.com>
2017-09-09 21:03:33 -0400

    Complete assignment (for practice)

--- anagrams.rb
+++ anagrams.rb
@@ -1,1 +1,26 @@
+# def word_sorter(str)
+#  	str.downcase.split('').sort
+# end
 
+# def anagrams?(word1, word2)
+# 	first_word = word_sorter(word1)
+# 	second_word = word_sorter(word2)
+
+# 	if first_word == second_word
+# 		return true
+# 	else
+# 		return false
+# 	end
+# end
+
+#Driver code test
+# anagrams?("leman", "melon")
+# anagrams?("lemon", "melon")
+
+def anagrams?(word, possible_anagram)
+  anagram_canonical_form(word) == anagram_canonical_form(possible_anagram)
+end
+
+def anagram_canonical_form(word)
+  word.downcase.split('').sort
+end
--- spec/anagrams_spec.rb
+++ spec/anagrams_spec.rb
@@ -1,4 +1,49 @@
 require_relative '../anagrams'
 
 describe 'anagrams?' do
+
+  it 'returns true if the two strings provided are anagrams' do
+    expect(anagrams?("melon", "lemon")).to eq (true)
+  end
+
+  it 'returns false if the two strings provided are not anagrams' do
+      expect(anagrams?("strawberry", "apple")).to eq (false)
+    end
+
+  it 'returns true if the two srings provided are anagrams and include capitalization' do
+    expect(anagrams?("MElon", "lemoN")).to eq (true)
+  end
+
+  it 'returns true if the two strings provided are anagrams regardless of word order' do
+    expect(anagrams?("lemon", "melon")).to eq (true)
+  end
+
+  it 'returns true the two strings provided are the same word' do
+    expect(anagrams?("lemon", "lemon")).to eq (true)
+  end
+
+  it 'returns true if the two strings provided contain non-English characters' do
+    expect(anagrams?("l3mon", "m3lon")).to eq (true)
+  end
+
 end
+
+# # Argument order does not matter.
+# anagrams?('melon', 'lemon')
+# # => true
+# anagrams?('lemon', 'melon')
+# # => true
+
+# # A word is an anagram of itself.
+# anagrams?('melon', 'melon')
+# # => true
+
+# # Anagrams are case-insensitive.
+# anagrams?('MELON', 'lemon')
+# # => true
+
+# # The arguments don't need to be valid English words.
+# anagrams?('abcde2', 'c2abed')
+# # => true
+# anagrams?('kilso', 'osilk')
+# # => true
